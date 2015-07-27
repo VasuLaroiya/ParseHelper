d62fc46d16529618adcf024d765731c26cf01081
Vasu Laroiya <vasularoiya@gmail.com>
2015-07-27 11:23:44 -0400

    Initial Commit

---
+++ ParseHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseHelper
+//
+//  Created by Vasu Laroiya on 7/27/15.
+//  Copyright (c) 2015 Vasu Laroiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
